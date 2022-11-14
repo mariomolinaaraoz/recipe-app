e3bc9e7833d273d2e7db2f56c7e4f68b837e6f59
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-11-14 12:46:47 -0300

    fix login route

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-import { Navigate } from 'react-router';
+import { useNavigate, NavLink } from 'react-router-dom'
 import { supabase } from "../supabase/client";
 
 function AuthOnly({ children }) {
 
+  const navigate = useNavigate();
 
   const [isAuthenticated] = useState(supabase.auth.user());
 
   /* If user is authenticated, the following line renders 
   the tags inside the AuthOnly Tag otherwise navigate the user to the auth view*/
 
-
-  return isAuthenticated ? children : <Navigate to='/login' />;
+  // <Navigate to='/login' />
+  return isAuthenticated ? children : navigate("/login");
 }
 
 export default AuthOnly;
