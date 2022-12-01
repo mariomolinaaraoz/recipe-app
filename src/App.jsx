06f6b8fa48dac797bfe7da3d8a73eaedf58f0234
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-12-01 15:51:58 -0300

    Vercel analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar, Footer } from './components'
 import { RecipeContextProvider } from "./context/Context";
 import MyRoutes from "./routes/Routes"
 import { CartProvider } from 'react-use-cart';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
 
@@ -10,10 +11,12 @@ export default function App() {
     <div className="App">
       <RecipeContextProvider>
           <CartProvider>
+            <Analytics />
               <header className="navbar navbar-expand-sm bg-second-color navbar-dark sm-fs">
                 <Navbar/>
               </header>
               <main>
+              <Analytics />
                 <MyRoutes/>
               </main>
               <footer>
