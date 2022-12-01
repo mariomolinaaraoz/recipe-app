a46411a6255bfe8cbfcd929ab37acaec05eae40c
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-12-01 13:08:52 -0300

    add web vitals

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
+import reportWebVitals from './reportWebVitals';
+import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +13,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+reportWebVitals(sendToVercelAnalytics);
